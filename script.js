caa4887ea4fc63461eaafeeb46b97639b5ff5fcd
Raphael <graphael.dr@gmail.com>
2023-05-26 02:23:49 -0600

    Added limit to number of digits displayed

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let display = document.querySelector('.display');
 display.innerHTML = (localStorage.getItem('calculation') || '');
 
 function updateCalculation(num) {
-  calculation += num;
-  display.innerHTML = calculation;
-  localStorage.setItem('calculation', calculation);
+  if(calculation.length<12){
+    calculation += num;
+    display.innerHTML = calculation;
+    localStorage.setItem('calculation', calculation);
+  }
 }
