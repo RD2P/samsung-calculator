1571cbaf0fe95fdf3c4c129e8cbb80158714eb40
Raphael <graphael.dr@gmail.com>
2024-01-02 17:49:22 -0600

    Cleanup

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let display = document.querySelector('.display');
 display.innerHTML = (localStorage.getItem('calculation') || '');
 
 function updateCalculation(num) {
-  if(calculation.length<12){
+  if(calculation.length < 12){
     calculation += num;
     display.innerHTML = calculation;
     localStorage.setItem('calculation', calculation);
