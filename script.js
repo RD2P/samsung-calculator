d11a5bd8956016ee3cb9432684300d01fb9adaab
Raphael <graphael.dr@gmail.com>
2024-01-02 19:00:38 -0600

    Add some error handling

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-let calculation = '';
-let display = document.querySelector('.display');
+let calculation = ''
+let stack = []
+let display = document.querySelector('.display')
 
-display.innerHTML = (localStorage.getItem('calculation') || '');
+display.innerHTML = (localStorage.getItem('calculation') || '')
 
-function updateCalculation(num) {
-  if(calculation.length < 12){
+const updateCalculation = (num) => {
+  if (calculation.length < 12) {
     calculation += num;
     display.innerHTML = calculation;
     localStorage.setItem('calculation', calculation);
   }
+}
+
+const handleParenthesis = () => {
+  if (stack.length % 2 === 0) {
+    calculation += '('
+    stack.push('(')
+  } else {
+    calculation += ')'
+    stack.push(')')
+  }
+  display.innerHTML = calculation;
+}
+
+const handleEquals = () => {
+  let parsed = []
+  for (let i = 0; i < calculation.length; i++) {
+    if (calculation[i] === '(' && calculation[i - 1] === ')') {
+      parsed.push('*')
+    }
+    parsed.push(calculation[i])
+  }
+
+  parsed = parsed.join('')
+
+  try {
+    calculation = eval(parsed);
+    display.innerHTML = calculation
+    localStorage.setItem('calculation', calculation)
+  } catch (error) {
+    display.innerHTML = 'Invalid input used'
+  }
+  stack = []
 }
